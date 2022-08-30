71390d6e391de436aa8eeb01df9a0ca4f78f2ef5
David Andersson <lejonmanen@gmail.com>
2022-08-30 15:10:43 +0200

    Dark mode

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,25 @@ import './App.css'
 function App() {
 	// useState är generisk, ange datatypen med <>
 	const [count, setCount] = useState<number>(0)
+	const [isDarkMode, setIsDarkMode] = useState<boolean>(false)
+	
+	// bra att tala om för TypeScript att darkCss är en sträng
+	const darkCss: string = isDarkMode ? ' dark' : ''
+
+	// inte lika meningsfullt att ange datatypen här
+	const handleClick: () => void = () => setCount((count) => count + 1)
+	const handleDarkModeClick = () => setIsDarkMode(mode => !mode)
 	
 	return (
-		<div className="App">
+		<div className={"App" + darkCss}>
 		
 			<h1>Vite + React</h1>
 			<div className="card">
-				<button onClick={() => setCount((count) => count + 1)}>
-			count is {count}
-			</button>
+				<button onClick={handleClick}>
+					count is {count}
+				</button>
+
+				<button onClick={handleDarkModeClick}> Toggla dark mode </button>
 			
 			</div>
 		
